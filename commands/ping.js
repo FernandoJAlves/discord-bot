21971df30687886451d8b7657ca6f2a9e14be9b2
Fernando Alves <fernando.alves@cldware.com>
2020-11-24 19:41:40 +0000

    Indented the code to 2 spaces

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,12 @@
 module.exports = {
-    name: 'ping',
-    description: 'A simple ping command',
-    execute(message, args) {
-        runCommand(message, args);
-    }
+  name: 'ping',
+  description: 'A simple ping command',
+  execute(message, args) {
+    runCommand(message, args);
+  }
 }
 
-
+// this is the entrypoint to the command
 function runCommand(message, args) {
-    message.channel.send('pong');
+  message.channel.send('pong');
 }
--- main.js
+++ main.js
@@ -11,32 +11,32 @@ const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith(
 
 // add all commands in 'commands' folder
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    botClient.commands.set(command.name, command);
+  const command = require(`./commands/${file}`);
+  botClient.commands.set(command.name, command);
 }
 
 
 botClient.once('ready', () => {
-    console.log("Nando's Bot is online");
+  console.log("Nando's Bot is online");
 });
 
 botClient.on('message', message => {
-
-    if (!message.content.startsWith(botPrefix) || message.author.bot) return; // ignore if it's not a command or a message from a bot
-
-    const commandArgs = message.content.slice(botPrefix.length).split(/ +/);
-    const command = commandArgs.shift().toLowerCase();
-
-    switch (command) {
-        case 'ping':
-            botClient.commands.get('ping').execute(message, commandArgs);
-            break;
-
-        default:
-            message.channel.send('u wot m8?');
-            break;
-    }
-
+  
+  if (!message.content.startsWith(botPrefix) || message.author.bot) return; // ignore if it's not a command or a message from a bot
+  
+  const commandArgs = message.content.slice(botPrefix.length).split(/ +/);
+  const command = commandArgs.shift().toLowerCase();
+  
+  switch (command) {
+    case 'ping':
+      botClient.commands.get('ping').execute(message, commandArgs);
+      break;
+    
+    default:
+      message.channel.send('u wot m8?');
+      break;
+  }
+  
 });
 
 
