d5709b4406b3a1e0bd996a5be168ee3e2aefed07
Fernando Alves <fernando.alves@cldware.com>
2021-02-14 00:42:57 +0000

    Added code written before taking a break, might need review

--- main.js
+++ main.js
@@ -8,11 +8,17 @@ const botPrefix = '.'; // prefix used for commands
 botClient.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js')); // only get commands in .js files
+const commandList = [];
 
 // add all commands in 'commands' folder
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   botClient.commands.set(command.name, command);
+  commandList.push({
+    name: command.name,
+    description: command.description,
+    visible: command.visible || true
+  });
 }
 
 
