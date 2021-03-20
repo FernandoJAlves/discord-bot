b8d2bf6a53af9f3ca6e230210ffebdf5b526ceb3
Fernando Alves <fernando.alves@cldware.com>
2021-03-20 16:51:13 +0000

    Changed bot image

--- commands/info.js
+++ commands/info.js
@@ -13,7 +13,8 @@ function runCommand(client, message, args, Discord) {
     .setTitle('Hello there')
     .setURL('https://www.youtube.com/watch?v=frszEJb0aOo')
     .setDescription("I'm an experimental bot, coded by some guy that really needs sleep")
-    .setImage('https://giantbomb1.cbsistatic.com/uploads/scale_medium/0/6087/2437349-pikachu.png')
+    // .setImage('https://giantbomb1.cbsistatic.com/uploads/scale_medium/0/6087/2437349-pikachu.png')
+    .setImage('https://images-ext-1.discordapp.net/external/Tvx7fNJ5bLvvKL3v0WhlPeC3HWkm27vlLTkpaRR4Ltk/http/wallpoper.com/images/00/41/43/46/pokemon-snorlax_00414346.png')
     .setFooter('Make sure to spam me with bugs');
 
   message.channel.send(newEmbed);
